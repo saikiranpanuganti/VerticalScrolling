19c6e5670943f778f8da2c7ff4ece3308e674f6f
SaiKiranPanuganti <saikiranpanuganti92@gmail.com>
2023-09-22 22:47:39 +0530

    Initial Commit

---
+++ VerticalScrolling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VerticalScrolling
+//
+//  Created by SaiKiran Panuganti on 22/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
